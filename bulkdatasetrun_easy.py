42337208ceaece137930a2017398efb5fec56cbb
Joyanta J. Mondal <hello@joyantamondal.com>
2021-10-21 13:34:10 +0600

    Add files via upload

--- bulkdatasetrun_easy.py
+++ bulkdatasetrun_easy.py
@@ -4,6 +4,9 @@ startTime = time.time()
 
 print("Let the game begin.")
 
+# firstly run the fileList.py file. 
+# and then getting the readme file (list of file), run this file
+
 f = open('readme.txt')
 
 arr = f.readlines()
@@ -12,14 +15,15 @@ import os
 
 for name in arr:
             idx = arr.index(name)
-            print(str(idx) + " out of " + str(len(arr)))
+            print(str(idx) + " out of " + str(len(arr)-1))
             name = name.strip('\n')
             print(name)
             # name = name[:-4] # I don't know why it was working with -4 but in dataset, it works with -5. it removes the .jpg part
             name = name[:-5]
             name_t = name+"_t.jpg" #progressive
             name_m = name+".jpg" #normal
-            subprocess.call(["jpegtran", "-optimize", "-progressive", "-scans", "script.txt", "-outfile", name_t, name_m])
+            scan_name = "script_final (compression)"
+            subprocess.call(["jpegtran", "-optimize", "-progressive", "-scans", f"{scan_name}.txt", "-outfile", name_t, name_m])
             print(name + " Done.")        
 
 print("All Done.")
